05092e74747f813f20840ea069753d15c29dcec6
Jules Fouchy <jules.fouchy@ntymail.com>
2022-02-27 15:26:54 +0100

    [glpp] Update

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -10,13 +10,13 @@ Texture::Texture(Interpolation minification_filter,
     bind_texture(_id);
     set_minification_filter(_id, minification_filter);
     set_magnification_filter(_id, magnification_filter);
-    set_horizontal_wrap(_id, horizontal_wrap);
-    set_vertical_wrap(_id, vertical_wrap);
+    set_wrap_s(_id, horizontal_wrap);
+    set_wrap_t(_id, vertical_wrap);
 }
 
 void Texture::bind() const
 {
-    bind_texture(*_id);
+    bind_texture(_id);
 }
 
 void Texture::bind_to_texture_unit(GLenum slot_idx) const
@@ -33,7 +33,7 @@ void Texture::resize(ImageSize size, TextureLayout layout)
 void Texture::upload_data(ImageSize size, const void* data, TextureLayout layout)
 {
     bind_texture(_id);
-    texture_image(*_id, layout.internal_format, size.width(), size.height(), layout.channels, layout.texel_data_type, data);
+    texture_image_2D(*_id, layout.internal_format, size.width(), size.height(), layout.channels, layout.texel_data_type, data);
     _size = size;
 }
 
